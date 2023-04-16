f31828862223113edf164d3bb0c6e096eba1d254
ericcnguyen04 <ericc.win@gmail.com>
2023-04-16 18:30:57 -0400

    road block

--- src/App.js
+++ src/App.js
@@ -6,10 +6,23 @@ function App() {
  const handlePlay = () => {
   console.log('play')
  }
+ const handleStop = () => {
+  console.log('play')
+ }
+ const handleNext = () => {
+  console.log('play')
+ }
+ const handlePrevious = () => {
+  console.log('play')
+ }
 
   return (
     <div className="App">
      <button onClick={handlePlay}>play</button>
+     <button onClick={handleStop}>stop</button>
+     <button onClick={handleNext}>next</button>
+     <button onClick={handlePrevious}>previous</button>
+
     </div>
   );
 }
